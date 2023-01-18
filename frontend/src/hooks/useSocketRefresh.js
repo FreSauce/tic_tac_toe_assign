a1d0cddaa32b4443cb0adce2c8148aac221ff3a8
Shardul Kurdukar <24shardul@gmail.com>
2023-01-18 18:54:04 +0530

    fixing socket logic

--- frontend/src/hooks/useSocketRefresh.js
+++ frontend/src/hooks/useSocketRefresh.js
@@ -1,9 +1,9 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import io from "socket.io-client";
-const socket = io(process.env.REACT_APP_API_URL);
 
 const useSocketRefresh = (callbackFn, gameId, userId) => {
+  const [socket, setSocket] = useState(io(process.env.REACT_APP_API_URL));
   const [isConnected, setIsConnected] = useState(socket.connected);
   useEffect(() => {
     socket.on("connect", () => {
@@ -13,14 +13,7 @@ const useSocketRefresh = (callbackFn, gameId, userId) => {
 
     socket.on("disconnect", () => {
       setIsConnected(false);
-    });
-
-    socket.on("user-connected", (userId) => {
-      console.log("user-connected " + userId);
-    });
-
-    socket.on("user-disconnected", (userId) => {
-      console.log("user-disconnected " + userId);
+      setSocket(io(process.env.REACT_APP_API_URL));
     });
 
     socket.onAny(() => {
@@ -28,17 +21,10 @@ const useSocketRefresh = (callbackFn, gameId, userId) => {
       callbackFn();
     });
 
-    // socket.on("game-update", () => {
-    //   console.log("gameUpdate");
-    //   callbackFn();
-    // });
-
-    // return () => {
-    //   socket.off("connect");
-    //   socket.off("disconnect");
-    //   socket.off("gameUpdate");
-    //   socket.off("user-connected");
-    // };
+    return () => {
+      socket.off("connect");
+      socket.off("disconnect");
+    };
   }, [gameId, userId]);
 
   return isConnected;
